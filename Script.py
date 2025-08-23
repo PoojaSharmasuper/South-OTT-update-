fec525451f1aede7fe90f97c99f27bafe31e43eb
PoojaSharmasuper <dhakadsonu196@gmail.com>
2025-08-23 12:04:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamcinezone">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/dreamcinezone>[Dreamcine_Botz]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Super4crazy78">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/super4crazy78>[@Super4crazy78]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
